a9289abb8c9d38f34df8c7c474cac220804f5fc5
hibaAkroush <hiba.akr@gmail.com>
2017-09-09 08:50:37 +0300

    initial modification still doesnt work

--- backbone-pond/fishModel.js
+++ backbone-pond/fishModel.js
@@ -7,6 +7,12 @@ var Fish = Backbone.Model.extend({
     image: 'http://www.google.com',
     description: 'Regular old fish',
     displayInfo: false
+  },
+  // i will make a function toggleDescription invoked from event
+  // this function should set the value of description in default
+  // to nothing when invoked
+  toggleDescription: function(){
+  	this.set("description",'')
   }
 
 });
